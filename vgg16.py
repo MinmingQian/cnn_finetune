e6c0bbc4c5ea2b48552dd3098517da14850031c6
fofore <qianminming@gmail.com>
2017-07-12 15:33:07 +0930

    fine

--- vgg16.py
+++ vgg16.py
@@ -82,7 +82,7 @@ def vgg16_model(img_rows, img_cols, channel=1, num_classes=None):
 
     # Learning rate is changed to 0.001
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
 
     return model
 
